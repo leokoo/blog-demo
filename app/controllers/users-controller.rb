a540ec250d314358c4af687fa3823f837d3309c8
Leo Koo <leokoo@gmail.com>
2015-12-02 14:55:26 +0800

    upload

--- app/controllers/users-controller.rb
+++ app/controllers/users-controller.rb
@@ -6,7 +6,7 @@ end
 # Create user
 post '/users' do
 	user = User.create(name: params[:name], email: params[:email], password: params[:password])
-	redirect '/users/"#{user.id}"'
+	redirect "/users/#{user.id}"
 end
 
 #Display user edit form
@@ -17,15 +17,29 @@ end
 
 # Update user
 patch '/users/:id' do
+	user = User.find(params[:id])
+	user.update(name: params[:name], email: params[:email], password: params[:password])
 end
 
 # Delete user profile
 delete '/users/:id' do
-
+	user = User.find(params[:id])
+	user.destroy
+	erb :"/static/index"
 end
 
 #View user
 get '/users/:id' do
 	@user = User.find(params[:id])
 	erb :"/user/show"
+end
+
+# view login page
+get '/users' do
+	erb :"/users/index"
+end
+
+# user login
+post 'users/login' do
+	user = User.find_by(email: params[:email], password: params[:password])
 end
